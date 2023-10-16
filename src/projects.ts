1f89aa70da4965e2089b983230339830c4ecf87a
bcanfield <bcanfield@miletwo.us>
2023-10-16 16:46:51 -0400

    Add some initial projects

--- src/projects.ts
+++ src/projects.ts
@@ -1,6 +1,7 @@
 import type { ImageProps } from '~/utils/images-optimization';
-import brandinImage from '~/assets/images/brandin-cartoon.png';
+import southpawImage from '~/assets/images/projects/southpaw.png';
 import wideImage from '~/assets/images/wide.png';
+import nexquikImage from '~/assets/images/projects/nexquik.png';
 
 export interface PersonalProject {
   title: string;
@@ -17,94 +18,25 @@ export interface ProfessionalProject {
 
 export const personalProjects: PersonalProject[] = [
   {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
+    title: 'Nexquik',
+    description:
+      'Package to auto-generate Next.js 13 server components from a Prisma Schema. It recursively traverses the database tree to generate all nested models, giving you instant ability to create, view, edit, and delete anything in your database.',
+    image: nexquikImage,
+    link: 'https://github.com/bcanfield/nexquik',
     technologies: [
+      { name: 'Next.js', icon: 'tabler:brand-nextjs' },
+      { name: 'Prisma', icon: 'tabler:brand-prisma' },
+
+      { name: 'Node', icon: 'tabler:brand-nodejs' },
       { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
     ],
   },
   {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
-  },
-  {
-    title: 'Project 1',
-    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: wideImage,
-    link: 'google.com',
-    technologies: [
-      { name: 'Typescript', icon: 'tabler:brand-typescript' },
-      { name: 'Docker', icon: 'tabler:brand-docker' },
-    ],
+    title: 'Southpaw',
+    description: `Library to authenticate with the private Fanduel API and query the authenticated user's data suche as upcoming contests, lineups, profile info, etc.`,
+    image: southpawImage,
+    link: 'https://github.com/bcanfield/southpaw',
+    technologies: [{ name: 'Python', icon: 'tabler:brand-python' }],
   },
 ];
 
