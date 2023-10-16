f500b734f8356d5b95c4ee74bf8dcb9097d0aea9
bcanfield <bcanfield@miletwo.us>
2023-10-16 16:16:49 -0400

    Styling

--- src/projects.ts
+++ src/projects.ts
@@ -1,10 +1,12 @@
 import type { ImageProps } from '~/utils/images-optimization';
 import brandinImage from '~/assets/images/brandin-cartoon.png';
+import wideImage from '~/assets/images/wide.png';
 
 export interface PersonalProject {
   title: string;
   description: string;
   image: ImageProps['src'];
+  link?: string;
   technologies: Array<{ name: string; icon?: string }>;
 }
 export interface ProfessionalProject {
@@ -17,7 +19,88 @@ export const personalProjects: PersonalProject[] = [
   {
     title: 'Project 1',
     description: 'asdf  afsdfasdf asdfasdf asdfasdf',
-    image: brandinImage,
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
+    technologies: [
+      { name: 'Typescript', icon: 'tabler:brand-typescript' },
+      { name: 'Docker', icon: 'tabler:brand-docker' },
+    ],
+  },
+  {
+    title: 'Project 1',
+    description: 'asdf  afsdfasdf asdfasdf asdfasdf',
+    image: wideImage,
+    link: 'google.com',
     technologies: [
       { name: 'Typescript', icon: 'tabler:brand-typescript' },
       { name: 'Docker', icon: 'tabler:brand-docker' },
